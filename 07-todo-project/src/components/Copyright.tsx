95ddad561f0914181941562bc4ba7ef54f10de92
mauropr99 <mauroparedesr@gmail.com>
2024-03-19 00:00:21 +1000

    First React + TypeScript project. TODO App

---
+++ 07-todo-project/src/components/Copyright.tsx
@@ -0,0 +1,8 @@
+import "./Copyright.css";
+
+export const Copyright: React.FC = () => (
+  <footer className="copyright">
+    <h4>React course</h4>
+    <h5>TODO with TypeScript</h5>
+  </footer>
+);
---
+++ 07-todo-project/src/components/Footer.tsx
@@ -0,0 +1,41 @@
+import { type FilterValue } from "../types";
+import { Filters } from "./Filters";
+
+interface Props {
+  handleFilterChange: (filter: FilterValue) => void;
+  activeCount: number;
+  completedCount: number;
+  onClearCompleted: () => void;
+  filterSelected: FilterValue;
+}
+
+export const Footer: React.FC<Props> = ({
+  activeCount,
+  completedCount,
+  onClearCompleted,
+  filterSelected,
+  handleFilterChange,
+}) => {
+  const singleActiveCount = activeCount === 1;
+  const activeTodoWord = singleActiveCount ? "task" : "tasks";
+
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>{activeCount}</strong> pending {activeTodoWord}
+        {!singleActiveCount && "s"}
+      </span>
+
+      <Filters
+        filterSelected={filterSelected}
+        handleFilterChange={handleFilterChange}
+      />
+
+      {completedCount > 0 && (
+        <button className="clear-completed" onClick={onClearCompleted}>
+          Clear completed
+        </button>
+      )}
+    </footer>
+  );
+};
